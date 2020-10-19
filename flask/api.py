917c0c7c7a97945b5f21e7a57795382036860dc0
andriygav <andriy.graboviy@mail.ru>
2020-10-19 21:44:48 +0300

    add reading data from database

--- flask/api.py
+++ flask/api.py
@@ -1,9 +1,91 @@
 # -*- coding: utf-8 -*- 
 import numpy as np
+import json
 
 from covidlib import approximator
 from copy import deepcopy
 
+import boto3
+class DynamoDBSingleton(object):
+    _dynamodb = None
+    
+    @staticmethod
+    def load():
+        DynamoDBSingleton._dynamodb = boto3.resource('dynamodb',
+                                                     region_name="us-west-2",
+                                                     endpoint_url="http://localhost:8000")
+        return DynamoDBSingleton._dynamodb
+    
+    @staticmethod
+    def get():
+        if DynamoDBSingleton._dynamodb is not None:
+            return DynamoDBSingleton._dynamodb
+        else:
+            return DynamoDBSingleton.load()
+
+def init_base():
+    dynamodb = DynamoDBSingleton.get()
+    try:
+        cities = dynamodb.create_table(
+            TableName='cities',
+            KeySchema=[
+                {
+                    'AttributeName': 'id',
+                    'KeyType': 'HASH'
+                },
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'id',
+                    'AttributeType': 'S'
+                },
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            }
+        )
+
+        np.random.seed(42)
+        x = np.linspace(2, 5, 30)
+        y = 100*(np.sin(10*x)+1)/x + np.random.randn(30)
+        DICT = dict()
+        for i in range(1, 30):
+            DICT[i] = dict()
+            DICT[i]['date'] = '{}.9.2020'.format(i)
+            DICT[i]['sick'] = y[i]
+            DICT[i]['recovered'] = np.random.randint(1, 100)
+            DICT[i]['died'] = np.random.randint(1, 100)
+
+        cities.put_item(
+            Item={'id': 'test1',
+                  'name': 'Тестовый город 1',
+                  'from': DICT[1]['date'],
+                  'to': DICT[29]['date'],
+                  'data': json.dumps(DICT)})
+
+
+        np.random.seed(0)
+        x = np.linspace(2, 5, 30)
+        y = (np.exp(x)+1)/x + np.random.randn(30)
+        DICT = dict()
+        for i in range(1, 30):
+            DICT[i] = dict()
+            DICT[i]['date'] = '{}.9.2020'.format(i)
+            DICT[i]['sick'] = y[i]
+            DICT[i]['recovered'] = np.random.randint(1, 100)
+            DICT[i]['died'] = np.random.randint(1, 100)
+                    
+                    
+        cities.put_item(
+            Item={'id': 'test2',
+                  'name': 'Тестовый город 2',
+                  'from': DICT[1]['date'],
+                  'to': DICT[29]['date'],
+                  'data': json.dumps(DICT)})
+    except:
+        cities = dynamodb.Table('cities')
+        pass
 
 def approximate(data, models, date):
     r"""
@@ -37,7 +119,10 @@ def approximate(data, models, date):
 
 
 def get_cities():
-    return ['test1', 'test2']
+    dynamodb = DynamoDBSingleton.get()
+    table = dynamodb.Table('cities')
+    ret = table.scan()['Items']
+    return {item['id']: item['name'] for item in ret}
 
 def get_models():
     models = dict()
--- flask/flask.sh
+++ flask/flask.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+PYTHONPATH=. python3 -c "from api import init_base; init_base()"
 PYTHONPATH=. gunicorn --bind 0.0.0.0:$1 --timeout=86400 server:app --worker-connections 1000 -w 2 --threads 4
