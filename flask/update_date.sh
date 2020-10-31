f068e52f5875284f98ca96fa474bc9ca44606a94
andriygav <andriy.graboviy@mail.ru>
2020-10-31 23:30:28 +0300

    fixed path in cron

--- flask/update_date.sh
+++ flask/update_date.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONPATH=. python3 -c "from api import init_base, update_data; init_base(); update_data();"
+cd /app && PYTHONPATH=. python3 -c "from api import init_base, update_data; init_base(); update_data();"
