bdcc6874917c463b3ad5e31c81432be680f8c6db
andriygav <andriy.graboviy@mail.ru>
2020-10-31 22:16:54 +0300

    fixed file path

--- flask/server.py
+++ flask/server.py
@@ -15,7 +15,7 @@ from api import (approximate, get_cities, get_data_field, get_models, get_dates,
 
 app = Flask(__name__)
 
-logging.basicConfig(filename=os.path.join(dump_path, '/logs/logs.log'),
+logging.basicConfig(filename='/logs/logs.log',
                     level=logging.INFO,
                     format='%(asctime)s.%(msecs)03d %(levelname)s:%(funcName)s:%(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
